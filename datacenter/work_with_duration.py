2114b599989c670d92eaeaeedef0b3fafc248b8a
nakompuktere <144929164+nakompuktere@users.noreply.github.com>
2025-03-19 17:09:25 +0300

    Улучшил функцию

--- datacenter/work_with_duration.py
+++ datacenter/work_with_duration.py
@@ -5,7 +5,10 @@ def get_duration(remaining_visitors):
     present_time = localtime()
     entered_at_time = localtime(remaining_visitors.entered_at)
     leaved_at_time = localtime(remaining_visitors.leaved_at)
-    delta = leaved_at_time - entered_at_time
+    if leaved_at_time == None:
+        delta = present_time - entered_at_time
+    else:
+        delta = leaved_at_time - entered_at_time
     return delta.seconds
 
 def format_duration(duration, hour=3600, minute=60):
